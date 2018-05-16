636d5f8dee0703ac8ee318967c63577f47339ca2
Rainer Sawitzki <rainer.sawitzki@javacream.org>
2018-05-16 13:40:05 +0200

    add foreachdemo

---
+++ org.javacream.training.java9.language/src/org/javacream/training/java8/language/functional/ForEachDemo.java
@@ -0,0 +1,22 @@
+package org.javacream.training.java8.language.functional;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		List<String> names = List.of("Hugo", "Emil", "Franz");
+		for (String name: names) {
+			//OLD
+		}
+		
+		names.forEach((String element) -> System.out.println(element));
+		names.forEach(element -> System.out.println(element));
+		Consumer<String> c = System.out::println;
+		names.forEach(c);
+		
+		
+		
+	}
+}
